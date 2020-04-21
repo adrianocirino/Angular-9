0a7e0760e6c1ef1a233797a64b377558ce0cafa7
adrianocirino <adriano.cirino.da.silva@gmail.com>
2020-04-20 21:43:53 -0300

    Foi criado o componente produto crud e a navegação entre componentes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { ProdutoCrudComponent } from './views/produto-crud/produto-crud.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'produtos',
+    component: ProdutoCrudComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
